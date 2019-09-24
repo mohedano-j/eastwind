b6485f735eec3a558c1658ae554b1560e1b67b5e
mohedano-j <jose.mohedano@sa.ucsb.edu>
2019-09-24 10:25:08 -0700

    Latest changes

--- Source/Eastwind.Web/ClientApp/src/index.tsx
+++ Source/Eastwind.Web/ClientApp/src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.js";
-import "../node_modules/jquery/src/jquery";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/jquery/dist/jquery.min.js";
 import "./index.css";
 import App from "./components/App";
-import configureStore from "./redux/store/configureStore";
+import configureStore from "./redux/store/configureStore.dev";
 import { Provider as ReduxProvider } from "react-redux";
 
 const store = configureStore();
